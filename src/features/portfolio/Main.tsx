b13c5407a5488c5b50518d513873b40584dfcc5e
codexsud <codexsud@gmail.com>
2025-03-22 06:17:30 +0530

    Refactored

---
+++ src/features/portfolio/Main.tsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <main className="center">
+      <h1 className="text-blue-500">Hello, World!</h1>
+    </main>
+  );
+};
+
+export default Main;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Layout from "@theme/Layout";
 import type { ReactNode } from "react";
+import Main from "../features/portfolio/Main";
 
 export default function Home(): ReactNode {
   return (
-    <Layout description="Description will go into a meta tag in <head />">
-      <main className="center">
-        <h1 className="text-blue-500">Hello, World!</h1>
-      </main>
+    <Layout description="A portfolio of SUD">
+      <Main />
     </Layout>
   );
 }
